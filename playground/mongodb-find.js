0abb8c14bdd39404b94c367a56b1a58449787bef
Melissa Wei <melissa.weiibm.com@melissas-mbp-2.raleigh.ibm.com>
2018-12-07 15:39:00 -0500

    Init commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,31 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').find({
+  //   _id: new ObjectID('5c0ac6067aaa4f95795a9fcc')
+  // }).toArray().then((docs) => {
+  //   console.log('Todos');
+  //   console.log(JSON.stringify(docs, undefined, 2));
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+  // db.collection('Todos').find().count().then((count) => {
+  //   console.log(`Todos count: ${count}`);
+  // }, (err) => {
+  //   console.log('Unable to fetch todos', err);
+  // });
+
+db.collection('Users').find({name: 'Melissa'}).toArray().then((docs) => {
+  console.log(JSON.stringify(docs, undefined, 2));
+});
+
+  // db.close();
+});

cbfed98d4e266b608b38bdaf0b3d5994f86db8cc
Melissa Wei <melissa.weiibm.com@melissas-mbp-2.raleigh.ibm.com>
2019-01-08 16:14:47 -0500

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,11 +1,14 @@
 //library imports
 var express = require('express');
 var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 //local imports
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+
+
 
 var app = express();
 
@@ -29,6 +32,33 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   });
+});
+
+//GET /todos/123445
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  // Validate id using isValid
+    //404 - send back empty
+  if (! ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  // findById
+    // success
+      // if todo - send it back
+      // if no todo - send back 404 with empty body
+    // error
+      //400 - send empty body back
+  Todo.findById(id).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+      res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+
 });
 
 app.listen(3000, () => {

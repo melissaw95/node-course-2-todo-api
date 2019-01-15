1481f26d2a8226885fccce11ff98de6b11bc5494
Melissa Wei <melissa.weiibm.com@melissas-mbp-2.raleigh.ibm.com>
2019-01-15 15:53:02 -0500

    Add PATCH /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 //library imports
-var express = require('express');
-var bodyParser = require('body-parser');
+const _ = require('lodash');
+const express = require('express');
+const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
 //local imports
@@ -86,7 +87,34 @@ app.delete('/todos/:id', (req, res) => {
     res.status(400).send();
   });
 
-});
+}); // end of delete
+
+app.patch('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  var body = _.pick(req.body, ['text', 'completed']);
+
+  if (! ObjectID.isValid(id)){
+    return res.status(404).send();
+  }
+
+  if (_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(id, {$set: body}, {new: true}).then((todo) => {
+    if (!todo) {
+      return res.status(404).send();
+    }
+
+    res.send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  })
+
+}); // end of patch
 
 app.listen(port, () => {
   console.log(`Started up at port ${port}`);
